54f55c2e907600f0e3beda1b3f96d0b10b03b736
melvinmarques1 <melvin.marques@ANHCAM04DR54W2.pitagoras.apollo.br>
2020-04-19 16:28:21 -0300

    Concerto de erro no value, e finalização da logica do projeto

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,11 +4,18 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
-const initialState= displayFunction()
-
+const initialState = {
+        displayValue: '0',
+        clearDisplay: false,
+        operation: null,
+        values: [0, 0],
+        current: 0  
+}
 export default class Calculator extends Component {
      
-        state = { ...initialState }
+        state = { 
+            ...initialState 
+        }
 
         constructor(props) {
             super(props)
@@ -22,7 +29,30 @@ export default class Calculator extends Component {
          }
 
          setOperation(operation) {
-             console.log(operation)
+            if ( this.state.current === 0 ) {
+                this.setState( {
+                  operation, current: 1, clearDisplay: true
+                } )
+              } else {
+                const equals = operation === '='
+                const currentOperation = this.state.operation
+                const values = [...this.state.values]
+                try {
+                  values[0] = eval( `${values[0]} ${currentOperation} ${values[1]}` )
+                } catch ( error ) {
+                  values[0] = this.state.values[0]
+                }
+                values[1] = 0
+           
+                this.setState( {
+                  displayValue: values[0],
+                  operation: equals ? null : operation,
+                  current: equals ? 0 : 1,
+                  clearDisplay: !equals,
+                  values
+                    
+                 })
+             }
          }
 
          addDigit(n){
@@ -40,7 +70,7 @@ export default class Calculator extends Component {
              if (n !== '.') {
                  const i = this.state.current
                  const newValue = parseFloat(displayValue)
-                 const values = [...this.state.values] 
+                 const values = [...this.state.values]
                  values[i] = newValue
                  this.setState({ values })
                  console.log(values)
@@ -59,7 +89,7 @@ export default class Calculator extends Component {
                 <Button label="7" click={this.addDigit}/>
                 <Button label="8" click={this.addDigit}/>
                 <Button label="9" click={this.addDigit}/>
-                <Button label="*" click={this.addDigit} operation/>
+                <Button label="*" click={this.setOperation} operation/>
                 <Button label="4" click={this.addDigit}/>
                 <Button label="5" click={this.addDigit}/>
                 <Button label="6" click={this.addDigit}/>
@@ -78,12 +108,5 @@ export default class Calculator extends Component {
     }
 }
 
-function displayFunction() {
-    return {
-        displayValue: '0',
-        clearDisplay: false,
-        operation: null,
-        value: [0, 0],
-        current: 0
-    }
-}
+
+
